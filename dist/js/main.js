5f0dade96fc33a33a8291837b89a5b3038f8b5af
Nenad Suknovic <nenad.suknovicpr143@gmail.com>
2023-06-01 13:09:44 +0200

    footer section and desktop version complete

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,3 +21,36 @@ var swiper = new Swiper('.swiper', {
         el: '.swiper-scrollbar',
     },
 });
+
+function nav() {
+    var $toggle = $('.js-toggle');
+    var open = 'toggle-open';
+    var $nav = $('.js-nav');
+
+    $toggle.on('click', function () {
+        if ($toggle.hasClass(open)) {  //hasClass - metoda za proveru postojanja klase
+            $toggle.removeClass(open);
+            $nav.stop().slideUp(function () {   //ovako pozivamo callback funkciju koja se izvrsava nakon sto prva zavrsi (u ovom slucaju nakon sto se uvuce navigacija).
+                $nav.removeAttr('style');
+            });  //metoda stop() nam omogućava da se klikne samo jednom a ne vise puta (sprecava yoyo efekat ponovnog zatvaranja i otvaranja)
+        } else {
+            $toggle.addClass(open);
+            $nav.stop().slideDown();
+        }
+    });
+}
+
+function inputOnFocus() {
+    var $input = $('.js-input');
+    var onFocus = 'on-focus';
+
+    $input.focus(function () {
+        $(this).addClass(onFocus);
+    });
+
+    $input.blur(function () {
+        $(this).removeClass(onFocus)
+    })
+}
+
+inputOnFocus();
