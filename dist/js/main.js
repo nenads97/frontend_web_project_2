7c7e4c4107c0a532d3c8fa400af2d2d0a4539fe3
Nenad Suknovic <nenad.suknovicpr143@gmail.com>
2023-06-05 18:40:27 +0200

    Responsive and swiper in progress

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,5 @@
+let borderUrl = `url(\'images/banner.png\')`;
+
 var swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: false,
@@ -21,6 +23,26 @@ var swiper = new Swiper('.swiper', {
         el: '.swiper-scrollbar',
     },
 });
+
+var init = false;
+var swiper1;
+function swiperCard() {
+    if (window.innerWidth <= 1209) {
+        if (!init) {
+            init = true;
+            swiper1 = new Swiper(".swiper1", {
+                direction: "horizontal",
+                slidesPerView: "auto",
+                centeredSlides: true,
+                spaceBetween: 100,
+            });
+        }
+    } else if (init) {
+        swiper1.destroy();
+        init = false;
+    }
+}
+
 
 function nav() {
     var $toggle = $('.js-toggle');
@@ -53,4 +75,36 @@ function inputOnFocus() {
     })
 }
 
+function addGradient(url) {
+    var $gradient = $('.js-gradient');
+
+    $(window).on('resize load', function () {    //osluškujemo širinu ekrana i svaki put kad smanjimo ili povećamo ekran trigeruje se ovaj događaj
+        if ($(window).width() <= 1024) {
+            $gradient.css('background', `linear-gradient(193.35deg, rgba(0, 3, 8, 0) 3.18%, rgba(0, 3, 8, 0.6) 56.87%), ${url}`);
+        } else {
+            $gradient.css('background', url)
+        }
+    });
+}
+
+function centerTable() {
+    var $swiper = $('.js-swiper');
+    var sw = 'swiper-wrapper'
+
+    $(window).on('resize load', function () {
+        if ($(window).width() <= 1209) {
+            $swiper.addClass(sw);
+            $('.swiper1').css('width', '585px')
+        }
+        else {
+            $swiper.removeClass(sw);
+        }
+    });
+}
+
+
 inputOnFocus();
+addGradient(borderUrl);
+centerTable();
+swiperCard();
+window.addEventListener("resize load", swiperCard);
